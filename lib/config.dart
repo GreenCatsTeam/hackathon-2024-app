90fbc8139e2100d77548959cca66dd0dfc44e6d2
vasez <vasezs@inbox.ru>
2024-05-05 11:24:46 +0400

    ports

--- lib/config.dart
+++ lib/config.dart
@@ -1,10 +1,10 @@
 class Config {
   static const String appName = "Приложение мусорки";
   static const String apiURL = "localhost:8080";
-  static const String flaskURL="localhost:5000";
+  static const String flaskURL="localhost:5050";
   static const String loginAPI = "/auth/login";
   static const String registerAPI = "/auth/registration";
-  static const String imageServerURL = "KARTINKI_SERVERd";
+  static const String imageServerURL = "localhost:5050";
   static const String uploadImg = "/upload_img";
   static const String getImg = "/get_img";
   static const String postCards="/cards";
